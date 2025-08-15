e8cbcf56ffbd81524d58dd045d94eee4470216e1
Mihir269 <gunemihir3@gmail.com>
2025-08-15 20:39:14 +0530

    Update task1-part1.py

--- task1-part1.py
+++ task1-part1.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pandas as pd
-c=0
 
-data=read_csv("order_metadata")
-for i in data['user_id']:
-  c++
-print(c)
-
+data=pd.read_csv(r"C:\Users\gunem\OneDrive\Desktop\IIT work\order_metadata.csv")
+num_orders = data.shape[0]
+print("num of orders",num_orders)
+abc=data.tail(1)
+num_customers=abc['user_id'].values
+print("num of customers", num_customers[0])
   
   
