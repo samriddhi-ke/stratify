097174bfefbe9c999953aaabf355fa219005e3af
Mihir269 <gunemihir3@gmail.com>
2025-08-15 21:11:18 +0530

    Update task1-part3.py

--- task1-part3.py
+++ task1-part3.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+data=pd.read_csv(r"c:\Users\gunem\OneDrive\Desktop\stratify documents\order_items__train.csv")
+
+product_count = data['product_id'].value_counts()
+print(product_count.sort_values(ascending=False).head(20))
